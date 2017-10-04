f3d84ae3aec6a78d80679eab14a858610039f687
Chrissy h Roberts <31275801+chrissyhroberts@users.noreply.github.com>
2017-10-04 12:29:46 +0100

    Update 000_master_control_script.sh

--- 000_master_control_script.sh
+++ 000_master_control_script.sh
@@ -5,9 +5,9 @@ read_two=$2
 short=$3
 
 #gunzip input/*.gz
-bwa mem chr6.fa ${read_one}  ${read_two} > input/${short}.sam
+bwa mem chr6/chr6.fa ${read_one}  ${read_two} > input/${short}.sam
 echo "bwa done"
-samtools view -bT chr6.fa input/${short}.sam > input/${short}.bam
+samtools view -bT chr6/chr6.fa input/${short}.sam > input/${short}.bam
 samtools sort input/${short}.bam > input/${short}.sorted.bam
 samtools index input/${short}.sorted.bam input/${short}.sorted.bai
 echo "done index and align"
